d0bb30a526fea0d5f17634209a1d1c689504f44d
gymcoding <bruce.lean17@gmail.com>
2022-03-02 11:48:55 +0900

    Init project

---
+++ b.js
@@ -0,0 +1,3 @@
+console.log('b.js:::1 > number: ', number);
+var number = 20;
+console.log('b.js:::2 > number: ', number);
---
+++ commonjs/index.js
@@ -0,0 +1,6 @@
+const { perfectScore, sum, avg, subtract } = require('./math');
+
+console.log('perfectScore: ', perfectScore);
+console.log('sum: ', sum(80, 10));
+console.log('avg: ', avg(80, 90));
+console.log('subtract: ', subtract(80, 90));
